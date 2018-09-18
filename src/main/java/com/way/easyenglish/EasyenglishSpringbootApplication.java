eef0f084ff7d4e19ea635210524015261bd4fb76
wayleung <Llw19941113#>
2018-09-18 17:07:33 +0800

    project init

---
+++ src/main/java/com/way/easyenglish/EasyenglishSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.way.easyenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyenglishSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyenglishSpringbootApplication.class, args);
+    }
+}
