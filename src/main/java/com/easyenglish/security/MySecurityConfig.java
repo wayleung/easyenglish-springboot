b7d7d8fe0cc47b8ecbc2fbe0547f8b36670d9c73
wayleung <Llw19941113#>
2018-09-20 17:56:40 +0800

    add spring security

---
+++ src/main/java/com/easyenglish/security/MySecurityConfig.java
@@ -0,0 +1,14 @@
+package com.easyenglish.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: Way Leung wayleung13@163.com
+ * @Date: 9/20/2018 17:08
+ * @Description:
+ */
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
